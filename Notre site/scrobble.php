a4ff7cb29206ce5beb66f1c24ba1e630e6d6f034
minisephirot <minisephirot@hotmail.fr>
2017-05-11 14:13:20 +0200

    Update scrobble.php

--- Notre site/scrobble.php
+++ Notre site/scrobble.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -16,6 +19,9 @@
       <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+        <style>
+.error {color: #FF0000;}
+</style>
   </head>
   <body>
 	  <nav class="navbar navbar-inverse navbar-static-top">
@@ -46,12 +52,148 @@
 							<li><a href="listeuser.php">Voir les Utilisateurs</a></li>
 						</ul>
 					</li>
-					<li><a href="login.php">Connexion</a></li>
+					<?php 
+					if (empty($_SESSION['pseudo'])){
+					echo '<li><a href="login.php">Connexion</a></li>';
+					}else{
+					echo '<li><a href="deconnexion.php">Deconnexion</a></li>';
+					}
+					?>
 				</ul>
 			</div>
 		</div>
 		</nav>
+					  <div class="corps">
+			<?php 
+					if (!empty($_SESSION['pseudo'])){
+					
+					    echo " <h2>Bonjour ".$_SESSION['pseudo'].", voici vos scrobble :</h2>";
+                    
+                $db="(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=oracle.depinfo.uhp-nancy.fr)(PORT=1521)))(CONNECT_DATA=(SERVICE_NAME=depinfo)))";
+		$user = "etud008";
+		$passwd = "jonathan";
 		
+		  // Check connection
+		  if($link = oci_connect("$user", "$passwd",$db)){
+
+			$stid = oci_parse($link, "SELECT * FROM SCROBBLING WHERE PSEUDO = '".$_SESSION['pseudo']."'");
+			oci_execute($stid);
+
+			echo "<table border='1'> <th>Nom d'artiste</th>   <th>Titre</th> <th>Pseudo</th> <th>Date</th>	\n";
+			while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS))
+			{
+			  echo "<tr>\n";
+			  foreach ($row as $item) {
+			    echo "    <td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "") . "</td>\n";
+			  }
+			  echo "</tr>\n";
+			}
+			echo "</table>\n";
+		  
+		  }else{
+		    $e = oci_error();   // Pour les erreurs oci_connect, aucun paramètre n'est passé
+		    echo htmlentities($e['message']);
+		    die();
+		  }
+		  oci_close($link);
+
+		  // define variables and set to empty values
+		    $nomArtisteErr = $titreErr = "";
+		    $nomArtiste = $titre = "";
+
+		    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+		      if (empty($_POST["nomArtiste"])) {
+			$nomArtisteErr = "Name is required";
+		      } else {
+			$nomArtiste = test_input($_POST["nomArtiste"]);
+			// check if name only contains letters and whitespace
+			if (!preg_match("/^[a-zA-Z ]*$/",$nomArtiste)) {
+			  $nomArtisteErr = "Only letters and white space allowed";
+			}
+		      }
+		      
+		      if (empty($_POST["titre"])) {
+			$titreErr = "titre is required";
+		      } else {
+			$titre = test_input($_POST["titre"]);
+			// check if e-mail address is well-formed
+			if (!preg_match("/^[a-zA-Z ]*$/",$titre)) {
+			  $titreErr = "Only letters and white space allowed";
+			}
+		      }
+
+		    }
+
+		    function test_input($data) {
+		      $data = trim($data);
+		      $data = stripslashes($data);
+		      $data = htmlspecialchars($data);
+		      return $data;
+		    }
+
+		/*    echo '
+		    <h2>Insertion de Scrobble</h2>
+		    <p><span class="error">* required field.</span></p>
+		    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+		      Nom artiste: <input type="text" name="nomArtiste" value="<?php echo $nomArtiste;?>">
+		      <span class="error">* <?php echo $nomArtisteErr;?></span>
+		      <br><br>
+		      Titre: <input type="text" name="titre" value="<?php echo $titre;?>">
+		      <span class="error">* <?php echo $titreErr;?></span>
+		      <br><br>
+		      <input type="submit" name="submit" value="Submit">  
+		    </form>';*/
+		    
+
+		    echo '<h2>Insertion de Scrobble</h2>
+		    <p><span class="error">* required field.</span></p>';
+		    echo '<form method="post" action="'.htmlspecialchars($_SERVER["PHP_SELF"]).'">';
+		    echo 'Nom artiste: <input type="text" name="nomArtiste" value="'.$nomArtiste.'">';
+		    echo '<span class="error">*'.$nomArtisteErr.'</span>';
+		    echo '<br><br>';
+		    echo 'Titre: <input type="text" name="titre" value="'.$titre.'">';
+		    echo '<span class="error">*'.$titreErr.'</span>';
+		    echo '<br><br>';
+		    echo '<input type="submit" name="submit" value="Submit">  
+		    </form>';
+
+		    echo "<br>";echo "<br>";
+		    if(isset($_POST['submit']))
+		    {
+		      echo "test";
+		      $db="(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=oracle.depinfo.uhp-nancy.fr)(PORT=1521)))(CONNECT_DATA=(SERVICE_NAME=depinfo)))";
+		      $user = "etud008";
+		      $passwd = "jonathan";
+			// Check connection
+			if($link = oci_connect("$user", "$passwd",$db)){
+			      $sql = "INSERT INTO SCROBBLING (NOM_ARTISTE,TITRE,PSEUDO) VALUES ('$nomArtiste','$titre','".$_SESSION['pseudo']."')";
+			      echo $sql;
+			      $stid = oci_parse($link, $sql);
+			      if(oci_execute($stid)){
+				      echo "Data inséré dans la base, veuillez rafraichir pour voir la BDD actualisée.";
+				      oci_commit($link);
+			      }else{
+				echo '<p class="error">Une erreur est survenue (nom auteur pas dans la liste ou média déja existant) </p>' ;
+			      }
+			      oci_close($link);
+			      header('Location: scrobble.php');
+			      exit;
+			}else{
+			  $e = oci_error();   // Pour les erreurs oci_connect, aucun paramètre n'est passé
+			  echo htmlentities($e['message']);
+			  die();
+			}
+			oci_close($link);
+			echo "</br> Connexion terminée et fermée </br>";
+		    } 
+					
+		}else{
+		  echo "Vous devez être connecté pour pouvoir scrobbler.";
+		  header( "refresh:5;url=login.php" );
+		}
+			?>
+				  		</div>
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
